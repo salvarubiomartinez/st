c82e2dfa32bed6fdd8834df0d2901cd2d9615605
Salvador Rubio <salvarubiomartinez@gmail.com>
2022-02-21 13:04:03 +0100

    background funcionality added

--- config.h
+++ config.h
@@ -13,8 +13,9 @@ static int borderpx = 2;
  * expects farbfeld format
  * pseudo transparency fixes coordinates to the screen origin
  */
-static const char *bgfile = "/path/to/image.ff";
-static const int pseudotransparency = 0;
+static const char *bgfile = "/home/retterblond/Apps/st/test.ff";
+static const int pseudotransparency = 1;
+
 
 /*
  * What program is execed by st depends of these precedence rules:
